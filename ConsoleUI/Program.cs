87913dc7f968af914a7fd72e8154a0e8a70a2f2a
Sude Sultansu <80518882+sdcsde@users.noreply.github.com>
2022-12-28 22:08:31 +0300

    Add files via upload

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            Car car = new Car();
+
+            car.CarId = 1;
+            car.BrandId = 2;
+            car.ColorId = 28;
+            car.DailyPrice = 500;
+            car.Description = "fast like ferrari";
+            car.ModelYear = 1969;
+
+            carManager.Add(car);
+
+
+        }
+    }
+}
